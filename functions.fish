5d643270b62e4aac29de17e9b454f2ff02be7cac
Félix Saparelli <felix@passcod.name>
2015-12-13 11:52:11 +1300

    Add tarsnap-all

--- functions.fish
+++ functions.fish
@@ -1,10 +1,6 @@
 function mkcd -d "Create a directory and cd to it"
-  if [ -z "$1" ]
-    echo "Usage: mkcd DIRNAME"
-  else
-    mkdir -p $1
-    cd $1
-  end
+  mkdir -p $1
+  cd $1
 end
 
 function ppwd -d "Save current CWD"
@@ -16,26 +12,35 @@ function ppcd -d "Go back to directory saved by ppwd"
 end
 
 function mkgit -d "Init a git repo in a new directory"
-  if [ -z "$1" ]
-    echo "Usage: mkgit DIRNAME"
-  else
-    mkcd $1
-    git init
-    git commit --allow-empty -m "Initial Commit"
+  mkcd $1
+  git init
+  git commit --allow-empty -m "Initial Commit"
+end
+
+function o -d "Open things"
+  set cmd "$argv"
+  if test -z "$cmd"
+    set cmd '.'
   end
+
+  if test ! -f "$cmd"
+    if ruby -e "exit '$cmd'.to_i"
+    else
+      set cmd "http://localhost:$cmd"
+    end
+  end
+
+  ruby -e "puts 'Opening ' + if '$cmd' == '.'; 'current directory'; else; '$cmd'; end + '...'"
+  xdg-open "$cmd" > /dev/null &
 end
 
 function set_proxy -d "Set the proxy settings"
-  if [ -z "$1" ]
-    echo "Usage: set_proxy <proxy>"
-  else
-    set -x HTTP_PROXY=$1
-    set -x http_proxy=$1
-    set -x HTTPS_PROXY=$1
-    set -x https_proxy=$1
-    set -x SOCKS_PROXY=$1
-    set -x socks_proxy=$1
-  end
+  set -x HTTP_PROXY=$1
+  set -x http_proxy=$1
+  set -x HTTPS_PROXY=$1
+  set -x https_proxy=$1
+  set -x SOCKS_PROXY=$1
+  set -x socks_proxy=$1
 end
 
 function unset_proxy -d "Unset the proxy settings"
